3cd41c0de3a68f1693b40cd914798fcbcd817545
浅井伸哉 <agu09z003@gmail.com>
2020-05-04 13:12:58 +0900

    エラーメッセージを日本語化する為修正

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  it "ユーザーネーム, メール,パスワードがあれば有効な状態であること" do
+
+  end
+
+  it "ユーザーネームが空であれば無効な状態であること" do
+
+  end
+
+  it "メールが空であれば無効な状態であること" do
+
+  end
+
+  it "パスワードが空であれば無効な状態であること" do
+
+  end
+
+  it "重複したユーザーネームは無効な状態であること" do
+
+  end
+
+  it "重複したメールは無効な状態であること" do
+
+  end
+end
